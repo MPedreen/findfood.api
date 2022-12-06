54cea5f89fdc0e197af04aea02d70d91a1fc4a4f
Pedro Ferreira <psouza@infisconsultoria.com.br>
2022-12-05 23:21:57 -0300

    feat: criação propriedades Estabelecimentos.

---
+++ FindFood.API/FindFood.Domain/Estabelecimentos/Estabelecimento.cs
@@ -0,0 +1,16 @@
+﻿using CoreBox.Domain;
+
+namespace FindFood.Domain.Estabelecimentos
+{
+    public partial class Estabelecimento : Entity<Estabelecimento>
+    {
+        public string Nome { get; private set; }
+        public string Logradouro { get; private set; }
+        public string Numero { get; private set; }
+        public string Cep { get; private set; }
+        public string Complemento { get; private set; }
+        public string Bairro { get; private set; }
+        public string Cidade { get; private set; }
+        public IEnumerable<Categoria> Categorias { get; set; }
+    }
+}
