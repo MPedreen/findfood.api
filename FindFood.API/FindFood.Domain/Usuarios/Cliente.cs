9278a07807a890420731035f0d4f198c47e92477
Pedro Ferreira <psouza@infisconsultoria.com.br>
2022-12-07 22:29:24 -0300

    feat: criação propriedades Cliente.

--- FindFood.API/FindFood.Domain/Usuarios/Cliente.cs
+++ FindFood.API/FindFood.Domain/Usuarios/Cliente.cs
@@ -4,6 +4,8 @@ namespace FindFood.Domain.Usuarios
 {
     public partial class Usuario : Entity<Usuario>
     {
-        
+        public string Cpf { get; private set; }
+        public string Genero { get; private set; }
+        public DateTime DataNascimento { get; private set; }
     }
 }
